540bfa0a8810bcf273972bc132a927522dc5305e
4lp <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2024-10-18 11:48:28 +0700

    Update Install.lua

--- Install.lua
+++ Install.lua
@@ -1,1 +1,7 @@
-
+local st,kl,tg,ka = loadstring,rawget,game.HttpGet,"https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Neuron-XYZ/refs/heads/main/Source/";
+local Ua,ctx,dir = setmetatable({[994732206]="BloxFruit.lua",[2440500124]="Doors.lua"},{__newindex = function(self,a,d)print('failed!');end,__metatable="The metatable is locked"}),game.GameId,error;
+if kl(Ua,ctx) then
+     st(tg(game,ka..kl(Ua,ctx)))({auth=true});
+else
+    dir("Failed to Execute\nUnsupported");
+end;
